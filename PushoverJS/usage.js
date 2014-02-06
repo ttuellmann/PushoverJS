00c5e0e92b0ebf9cd0ff2ceac4ce0bce500e3258
Richard Niemand <niemand.richard@gmail.com>
2014-02-06 21:05:59 +0200

    Initial client release

---
+++ PushoverJS/usage.js
@@ -0,0 +1,16 @@
+﻿// todo: complete usage examples
+var client1 = new PushoverJs(pushoverConfig.appToken, pushoverConfig.userKey);
+
+client1.createMessage()
+  .title('Message Title')
+  .message('Hello World')
+  .url('http://www.google.com/', 'Google')
+  .highPriority()
+  .addCurrentTime()
+  .playSound(client1.sounds.siren)
+  .pre()
+  .send();
+
+setTimeout(function () {
+  console.log('hrere');
+}, 4000);
